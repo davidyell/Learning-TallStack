87ff8fd78ed9efd216ef8472270463051de7457c
David Yell <david.yell@asset.tv>
2024-03-15 16:26:08 +0000

    Add types for easier type and property inference

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -6,8 +6,14 @@ namespace App\Models;
 
 use App\Casters\InningsArrayCaster;
 use App\Casters\MatchInfoCaster;
+use App\DTOs\Innings;
+use App\DTOs\MatchInfo;
 use MongoDB\Laravel\Eloquent\Model;
 
+/**
+ * @property MatchInfo $info
+ * @property Innings $innings
+ */
 class Game extends Model
 {
     protected $collection = 'matches';
